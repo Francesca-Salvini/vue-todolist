c26bbdaaeec738e8d0826b2acf2b7898d36df27b
Francesca Salvini <franci98sp@gmail.com>
2021-04-21 16:42:13 +0200

    completato vue js e html

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 var app = new Vue (
     {
         el : '#root',
-        data: {} ,
-        methods: {} 
-    }
+        data: {
+            userNewValue : '',
+            to_dos : [
+                'Fare i compiti',
+                'Fare la spesa',
+                'Fare il bucato'
+            ],
+        } ,
+        methods: {
+             // Aggiunge un nuovo todo all'array todos
+             addNewTodo() {
+                // se l'utente non compila ma clicca aggiungi, non succede nulla (non pusha)
+                if(this.userNewValue.length > 0 ){
+                    this.to_dos.push(this.userNewValue);
+                this.userNewValue = '';
+                }
+                
+            },
+
+            // Rimuove un elemento dall'array todos
+            deleteTodo(index) {
+                this.to_dos.splice(index,1);
+            }
+        }
+    } 
+    
     
 );
